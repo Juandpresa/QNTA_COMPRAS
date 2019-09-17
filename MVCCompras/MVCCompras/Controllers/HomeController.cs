1b14e5ae5ff701f1632d5eab49176fda864620dd
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-09-17 11:46:42 -0500

    Login y Opciones menu 100%

--- MVCCompras/MVCCompras/Controllers/HomeController.cs
+++ MVCCompras/MVCCompras/Controllers/HomeController.cs
@@ -12,16 +12,20 @@ namespace MVCCompras.Controllers
 {
   public class HomeController : Controller
   {
+    //crear objeto que tendra los valores de nuestro modelo de datos (MVCCompras.Models)
     private ComprasEntities db = new ComprasEntities();
     
     public ActionResult Index()
     {
+      //Validar que la sesion no este vacia
       if (Session["idUsuario"] != null)
       {
+        //Si sesion trae datos permite el acceso a la vista
         return View();
       }
       else
       {
+        //Si sesion es null redirecciona a la vista de login
         return RedirectToAction("Login");
       }
 
@@ -42,17 +46,26 @@ namespace MVCCompras.Controllers
       //Verificar que Correo y contraseña no esten vacio
       if (modelo.Correo != null && modelo.Pass != null)
       {
+        //Consulta a la base de datos para obtener los datos correspondientes y almacenarlos en el objeto user
         var user = db.Usuarios.FirstOrDefault(e => e.Correo == modelo.Correo && e.Pass == modelo.Pass);
+        //Verificar que user no sea null
         if (user != null)
         {
-          //Encontro usuario con los datos
+          //Guardar los datos en variables de sesion
+
+          //En sesion idUsuario guardar lo que tenga objeto user en el campo IdUsuario y convertirlo a string   
           Session["idUsuario"] = user.idUsuario.ToString();
+          //En sesion Correo guardar lo que tenga objeto user en el campo Correo y convertirlo a string
           Session["Correo"] = user.Correo.ToString();
+          //En sesion idTipoUsuario guardar lo que tenga objeto user en el campo IdTipoUsuario y convertirlo a string
+          Session["idTipoUsuario"] = user.idTipoUsuario.ToString();
+          //Redirecciona a la vista Index
           return RedirectToAction("Index");
 
         }
         else
         {
+          //Si user es null regresar a la vista Login
           return View("Login");
         }
       }
