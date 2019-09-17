4a13e2de01f3cb57220baefe47858996c6e4e750
ALEONRAMOS <55206685+ALEONRAMOS@users.noreply.github.com>
2019-09-17 11:41:52 -0500

    CRUDS Y MENUS

---
+++ MVCCompras/MVCCompras/Controllers/BancosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCCompras.Models;
+
+namespace MVCCompras.Controllers
+{
+    public class BancosController : Controller
+    {
+        private ComprasEntities db = new ComprasEntities();
+
+        // GET: Bancos
+        public ActionResult Index()
+        {
+            return View(db.Bancos.ToList());
+        }
+
+        // GET: Bancos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bancos bancos = db.Bancos.Find(id);
+            if (bancos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bancos);
+        }
+
+        // GET: Bancos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bancos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BancoId,Alias,Nombre,EstaActivo")] Bancos bancos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bancos.Add(bancos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bancos);
+        }
+
+        // GET: Bancos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bancos bancos = db.Bancos.Find(id);
+            if (bancos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bancos);
+        }
+
+        // POST: Bancos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BancoId,Alias,Nombre,EstaActivo")] Bancos bancos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bancos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bancos);
+        }
+
+        // GET: Bancos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bancos bancos = db.Bancos.Find(id);
+            if (bancos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bancos);
+        }
+
+        // POST: Bancos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bancos bancos = db.Bancos.Find(id);
+            db.Bancos.Remove(bancos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
