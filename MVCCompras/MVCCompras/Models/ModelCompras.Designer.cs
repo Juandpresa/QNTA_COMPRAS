b8e9168082a9db4a30fafc3d7028087da58f987c
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-09-18 10:01:30 -0500

    agregar campo Token_Recuperacion a tabla de usuarios

--- MVCCompras/MVCCompras/Models/ModelCompras.Designer.cs
+++ MVCCompras/MVCCompras/Models/ModelCompras.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Carlos Pagaro\source\repos\MVCCompras\MVCCompras\Models\ModelCompras.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\raymundo.ayala\source\repos\QNTA_COMPRAS\MVCCompras\MVCCompras\Models\ModelCompras.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
