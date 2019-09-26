c29a5436a8b37e62684666e03153e91e2a396b29
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-09-26 17:10:51 -0500

    comienzo validaciones

--- MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
+++ MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
@@ -12,24 +12,51 @@ namespace MVCCompras.Models
     public USolicitante Solicitantes { get; set; }
   }
   public class SolicitudMetaData
-  {
-    
+  {    
     public int SolicitudID { get; set; }
+    
+    [Required(ErrorMessage = "Selecciona una Opcion")]
+    [Display(Name = "Beneficiario")]
     public int ProveedorID { get; set; }
+
+    
+    [Required(ErrorMessage = "Selecciona una Opcion")]
+    [Display(Name = "Forma Pago")]
+    [MaxLength(50)]
     public int FormaPagoID { get; set; }
+
+
+    [Required(ErrorMessage = "Requerido")]
     public int TipoGastoID { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public int PeriocidadID { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public int CantidadPagos { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public decimal ImporteTotal { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public string ImporteLetra { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
+    [Display(Name = "Observaciones")]
     public string Observacion { get; set; }
+
+
     public System.DateTime FechaRegistro { get; set; }
     public System.DateTime FechaInicioPagos { get; set; }
     public System.DateTime FechaModificacion { get; set; }
     public string CuentaIDModificacion { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public Nullable<int> PagadoraID { get; set; }
     public string ObservacionesOtroFormaP { get; set; }
     public string ObsOtroTipoGasto { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public string Solicitante { get; set; }
   }
 }
