9c4a95a24122761e3996439e337c062018c10894
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-09-27 13:46:27 -0500

    solo numeros en importe

--- MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
+++ MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
@@ -19,10 +19,12 @@ namespace MVCCompras.Models
     [Display(Name = "Proveedor")]
     public int ProveedorID { get; set; }
 
-    
+    [Required]
+    [Display(Name = "Forma de Pago")]
     public int FormaPagoID { get; set; }
 
-    
+    [Required]
+    [Display(Name = "Tipo de Gasto")]
     public int TipoGastoID { get; set; }
     //public int PeriocidadID { get; set; }
     //public int CantidadPagos { get; set; }
@@ -35,19 +37,21 @@ namespace MVCCompras.Models
     [Display(Name = "Importe Letra")]
     public string ImporteLetra { get; set; }
 
-
-    //public string Observacion { get; set; }
+    
+    public string Observacion { get; set; }
     //public System.DateTime FechaRegistro { get; set; }
     //public System.DateTime FechaInicioPagos { get; set; }
     //public System.DateTime FechaModificacion { get; set; }
     //public string CuentaIDModificacion { get; set; }
 
-   
+    [Required]
+    [Display(Name = "Razón Social Pagadora")]
     public Nullable<int> PagadoraID { get; set; }
     //public string ObservacionesOtroFormaP { get; set; }
     //public string ObsOtroTipoGasto { get; set; }
 
-    
+    //[Required]
+    //[Display(Name = "Solicitante")]
     public string Solicitante { get; set; }
   }
 }
