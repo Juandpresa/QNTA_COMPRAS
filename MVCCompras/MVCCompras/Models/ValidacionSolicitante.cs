0ccc4e3051a594562893e48b76e06d0cef54faa1
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-09-27 10:40:47 -0500

    intento validaciones solicitud

--- MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
+++ MVCCompras/MVCCompras/Models/ValidacionSolicitante.cs
@@ -11,24 +11,40 @@ namespace MVCCompras.Models
   {
     public USolicitante Solicitantes { get; set; }
   }
+
   public class SolicitudMetaData
-  {    
-    public int SolicitudID { get; set; }
+  {
+    //public int SolicitudID { get; set; }
+
+    
     public int ProveedorID { get; set; }
+
+    
     public int FormaPagoID { get; set; }
+
+    
     public int TipoGastoID { get; set; }
-    public int PeriocidadID { get; set; }
-    public int CantidadPagos { get; set; }
+    //public int PeriocidadID { get; set; }
+    //public int CantidadPagos { get; set; }
+    
     public decimal ImporteTotal { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public string ImporteLetra { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
     public string Observacion { get; set; }
-    public System.DateTime FechaRegistro { get; set; }
-    public System.DateTime FechaInicioPagos { get; set; }
-    public System.DateTime FechaModificacion { get; set; }
-    public string CuentaIDModificacion { get; set; }
+    //public System.DateTime FechaRegistro { get; set; }
+    //public System.DateTime FechaInicioPagos { get; set; }
+    //public System.DateTime FechaModificacion { get; set; }
+    //public string CuentaIDModificacion { get; set; }
+
+   
     public Nullable<int> PagadoraID { get; set; }
-    public string ObservacionesOtroFormaP { get; set; }
-    public string ObsOtroTipoGasto { get; set; }
+    //public string ObservacionesOtroFormaP { get; set; }
+    //public string ObsOtroTipoGasto { get; set; }
+
+    
     public string Solicitante { get; set; }
   }
 }
