4b40a391037d7b0421b6e5dc3d8590aa72ff2bbc
Juandpresa <55206902+Juandpresa@users.noreply.github.com>
2019-10-04 14:15:52 -0500

    intento mostrar estatus

--- MVCCompras/MVCCompras/Controllers/SolicitudsController.cs
+++ MVCCompras/MVCCompras/Controllers/SolicitudsController.cs
@@ -30,14 +30,17 @@ namespace MVCCompras.Controllers
 
     public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
     {
+      //Consulta join para obtener el ALIAS  de la pagadora 
       var paga = (from p in db.Pagadora join s in db.Solicitud 
                   on p.PagadoraID equals s.PagadoraID
                   select new{ p.Alias});
+      //ciclo que asigna a la variable "cont" el tamaño de un arreglo que sera similar al tamaño de las tuplas que trae paga
       int cont = 0;
       foreach (var item in paga)
       {
         cont++;
       }
+      //foreach que asigna los valores de la consulta y los guarda en el arreglo pag
       int pagadora = 0;
       string[] pag = new string[cont];
       foreach (var item in paga)
@@ -45,16 +48,38 @@ namespace MVCCompras.Controllers
         pag[pagadora] = item.Alias;
         pagadora = pagadora + 1;
       }
+      //asignar a ViewData el cvalor del arreglo
       ViewData["pagadora"] = pag;
-    
 
-
+      //Consulta join para obtener el Status de solicitud
       var estat = (from s in db.Solicitud
                   join e in db.Seguimiento
                   on s.SolicitudID equals e.SolicitudID
                   join d in db.Estatus
                   on e.EstatusID equals d.EstatusId
                   select new { d.Nombre });
+
+      //ciclo que asigna a la variable "cont" el tamaño de un arreglo que sera similar al tamaño de las tuplas que trae estat
+      int conta = 0;
+      foreach (var item in estat)
+      {
+        conta++;
+      }
+      //foreach que asigna los valores de la consulta y los guarda en el arreglo pag
+      int statu = 0;
+      string[] status = new string[conta];
+      foreach (var item in estat)
+      {
+        status[statu] = item.Nombre;
+        statu = statu + 1;
+      }
+      //asignar a ViewData el cvalor del arreglo
+      ViewData["status"] = status;
+
+
+
+
+
       foreach (var item in estat)
       {
         string test = item.Nombre;
